35d38faf556dc7b322761e7a4c6b9e0b9e27c617
Victor Bratko <bratko.viktor2004@gmail.com>
2022-12-08 22:01:38 +0300

    stepik_lab_4 version 1.2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 using namespace std;
 
@@ -54,7 +55,7 @@ string function2(string str){
     string strRes = "";
     cout << "String before editing:" << "\n" << str << "\n";
     bool flag = true;
-    for (int i = 0; i <= str.length(); i++){
+    for (int i = 0; i < str.length(); i++){
         if (str[i] == ' '){
             if (flag){
                 continue;
@@ -75,6 +76,9 @@ string function2(string str){
                 i+=2;
                 strRes += "...";
                 continue;
+            }else if(str[i] == '.') {
+                strRes += '.';
+                break;
             }
             if(str[i] != str[i-1]){
                 strRes += str[i];
@@ -82,7 +86,80 @@ string function2(string str){
         }
     }
     cout << "String after editing:" << "\n" << strRes;
+    return strRes;
+}
 
+void function5(string str){
+    string subString = "", strRes = "not exist";
+    int count = 0;
+    cout << "Input the substring you want to find in the string:";
+    getline(cin, subString);
+    cout << "Linear search:" << "\n";
+    auto start = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    for(int i = 0; i < str.length() - subString.length(); i++){
+        string str1 = "";
+        for(int j = i; j < subString.length() + i; j++){
+            if (str[j] == subString[j-i]){
+                str1 += str[j];
+            } else {
+                continue;
+            }
+        }
+        if (subString == str1){
+            strRes = "exist";
+            count += 1;
+        }
+    }
+    auto end = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    cout << "SubString - " << strRes << "; There is(are) " << count << " substring(s);" << "Spent time: " << end - start << "ns\n";
+    cout << "Boyer moore algorithm:" << "\n";
+    strRes = "not exist"; count = 0;
+    int arr[subString.length()];
+    start = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    for(int i = 0; i < subString.length(); i++){
+        if(i != subString.length() - 1){
+            arr[i] = subString.length() - 1 - i;
+        }else{
+            bool flag = false;
+            for (int j = i - 1; j >= 0; j--){
+                if (subString[j] == subString[i]){
+                    flag = true;
+                    arr[i] = arr[j];
+                    break;
+                }
+            }
+            if(!flag){
+                arr[i] = subString.length();
+            }
+        }
+    }
+    for(int i = subString.length() - 1; i < str.length();){
+        string str1 = "";
+        bool flag = true;
+        if(subString.find(str[i]) != std::string::npos){
+            if (str[i] == subString[subString.length() - 1]){
+                for(int j = i; j > i - subString.length(); j--){
+                    if(str[j] == subString[subString.length() - 1 - i + j]){
+                        str1 = str[j] + str1;
+                    }else{
+                        flag = false;
+                        continue;
+                    }
+                }
+                if(flag){
+                    strRes = "exist";
+                    count += 1;
+                }
+                i += arr[subString.length() - 1];
+            }else{
+                i += arr[subString.find_last_of(str[i])];
+            }
+        }else{
+            i += subString.length();
+        }
+    }
+    end = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    cout << "SubString - " << strRes << "; There is(are) " << count << " substring(s);" << "Spent time: " << end - start << "ns\n";
 }
 
 int main() {
@@ -95,6 +172,13 @@ int main() {
     //·удалить лишние пробелы;
     //·удалить лишние знаки препинания (под «лишними» подразумевается несколько подряд идущих знаков;
     //·исправить регистр букв, если это требуется (пример некорректного использования регистра букв: пРиМЕр);
-    function2(str);
+    str = function2(str);
+    cout << "\n";
+    //3.
+
+    //4.
+
+    //5.
+    function5(str);
     return 0;
 }
